6d5379fed6fbcdff50d95d794de217c0f8793372
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2024-02-05 21:11:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ PICS = (environ.get('PICS' ,'https://graph.org/file/c3376ab143f7ded215412.jpg'))
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1782834874').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1782834874 1119579816 6812448168').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002015857012').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
